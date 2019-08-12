9fb7033c08d1ce9e89512d2e8c40bff63535f33b
rpcopopqpq@gmail.com <rpcopopqpq@gmail.com>
2019-08-12 11:14:02 +0900

    mms table 추가

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -6,3 +6,22 @@ CREATE TABLE T_TEST (
   create_date datetime NOT NULL,
   PRIMARY KEY (id)
 );
+
+
+CREATE TABLE MMS (
+                   MRN VARCHAR(255) NOT NULL,
+                   IP VARCHAR(39) NOT NULL,
+                   PORT SMALLINT UNSIGNED NOT NULL,
+                   CREATE_DATE DATETIME NOT NULL,
+                   UPDATE_DATE DATETIME,
+                   PRIMARY KEY(MRN)
+);
+
+CREATE TABLE ENTITY_HOME_MMS (
+                   MRN VARCHAR(255) NOT NULL,
+                   MRN_MMS VARCHAR(255) NOT NULL,
+                   CREATE_DATE DATETIME NOT NULL,
+                   UPDATE_DATE DATETIME,
+                   PRIMARY KEY(MRN),
+                   FOREIGN KEY (MRN_MMS) REFERENCES MMS (MRN)
+);
