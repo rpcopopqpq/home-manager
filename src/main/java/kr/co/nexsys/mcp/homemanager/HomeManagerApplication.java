8028299293cfd67a517eec5102611f3b43a4e5c2
ish128 <ish128@naver.com>
2019-10-27 16:17:51 +0900

    imsi for test

--- src/main/java/kr/co/nexsys/mcp/homemanager/HomeManagerApplication.java
+++ src/main/java/kr/co/nexsys/mcp/homemanager/HomeManagerApplication.java
@@ -3,9 +3,9 @@ package kr.co.nexsys.mcp.homemanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @ServletComponentScan
 @SpringBootApplication
@@ -14,6 +14,22 @@ public class HomeManagerApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(HomeManagerApplication.class, args);
+    }
+
+    @Bean
+    public CommonsRequestLoggingFilter commonsRequestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeHeaders(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setIncludeClientInfo(true);
+
+        loggingFilter.setBeforeMessagePrefix("Before : ");
+        loggingFilter.setBeforeMessageSuffix("");
+        loggingFilter.setAfterMessagePrefix("After : ");
+        loggingFilter.setAfterMessageSuffix("");
+
+        return loggingFilter;
     }
 
 }
